dc86702f3599a3909e2e4fd5a68782dacf69e4e2
Jerry Xu <xujianwei919@126.com>
2015-06-23 09:43:27 +0800

    no message

---
+++ yke/yke/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yke
+//
+//  Created by 于凯文 on 15/6/23.
+//  Copyright (c) 2015年 于凯文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ yke/yke/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  yke
+//
+//  Created by 于凯文 on 15/6/23.
+//  Copyright (c) 2015年 于凯文. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
